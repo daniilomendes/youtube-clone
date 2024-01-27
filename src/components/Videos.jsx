71bd51f43a20f0a9eed669e3c49dfa3916a87f35
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-27 17:26:30 -0300

    feat: adicionando o Loader antes de carregar os videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,10 +1,19 @@
-import { Box, Stack } from "@mui/material";
 import React from "react";
-import { VideoCard, ChannelCard } from "./";
+import { Stack, Box } from "@mui/material";
 
-const Videos = ({ videos }) => {
+import { ChannelCard, Loader, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return <Loader />;
+
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack
+      direction={direction || "row"}
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
